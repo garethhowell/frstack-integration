ee59273b8443884dcbee717e1e4673bca72a6857
Gareth Howell <gareth.howell@gmail.com>
2024-11-25 12:50:13 +0000

    Cookiecutter output

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for FRStack tests."""
+from custom_components.frstack.const import (
+    CONF_PASSWORD,
+)
+from custom_components.frstack.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
